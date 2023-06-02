370bb78182991aaf06b86f0b4ebbd29e2f4348cf
unknown <allandiamantedesouza@gmail.com>
2023-06-02 18:19:43 -0300

    arrumando build 7

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ echo "Building the project..."
 pip install -r requirements.txt
 
 echo "Make Migration..."
-python3 manage.py makemigrations --noinput
-python3 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 echo "Collect Static..."
-python3 manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
