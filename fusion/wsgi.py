9371b05af6fd743c8ff292ba43bd86ba4d76dc44
unknown <allandiamantedesouza@gmail.com>
2023-06-02 18:36:40 -0300

    arrumando build 12

--- fusion/wsgi.py
+++ fusion/wsgi.py
@@ -14,6 +14,6 @@ from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fusion.settings')
 
-application = Cling(MediaCling(get_wsgi_application()))
+application = get_wsgi_application()
 
 app = application
