c1ca16a0ca3e1410c50b40db4b2966cfa9318887
unknown <allandiamantedesouza@gmail.com>
2023-06-03 18:09:20 -0300

    arrumando build 42

--- fusion/settings.py
+++ fusion/settings.py
@@ -106,14 +106,14 @@ WSGI_APPLICATION = 'fusion.wsgi.app'
 # }
 
 
-DATABASES['default'] = dj_database_url.config()
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
