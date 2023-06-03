bf6bb3cf452cd007b868c13b7afc2a16dbc1e829
unknown <allandiamantedesouza@gmail.com>
2023-06-03 17:55:53 -0300

    arrumando build 38

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-python3.9 pip install -r requirements.txt
+python3 pip install -r requirements.txt
 
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
 
 python3 manage.py collectstatic --no-input
