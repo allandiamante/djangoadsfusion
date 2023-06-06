a213d6871b3530bf0b5a0983a35c03a08739b60b
unknown <allandiamantedesouza@gmail.com>
2023-06-06 10:14:15 -0300

    arrumando build 58

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ pip install -r requirements.txt
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 
-echo("Criou o statico")
+echo "Criou o statico"
 python3 manage.py collectstatic --no-input
